0a6fe3e998ae6549f326ce433e0f36c6c456a0c8
huyhoangse97 <80676594+huyhoangse97@users.noreply.github.com>
2021-06-03 10:53:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/drawingshape/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.drawingshape;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv_type2 = findViewById(R.id.tv_type2);
+    }
+}
